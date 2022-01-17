0056a1ca5bfd49b7914ec6aa0c9f94bac91c45df
wellington <wellington281482@gmail.com>
2022-01-16 23:44:57 -0300

    removida uma linha

--- allGuest.py
+++ allGuest.py
@@ -16,3 +16,4 @@ print(' - Cups ' + str(totalBrought(allGuests, 'cups')))
 print(' - Cakes ' + str(totalBrought(allGuests, 'cakes')))
 print(' - Ham Sandwiches ' + str(totalBrought(allGuests, 'ham sandwiches')))
 
+#
